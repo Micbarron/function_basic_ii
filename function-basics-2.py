60744de0b6ac8f0f2068b15cadddd36ce1c753b4
Michael Barron <michael.barron08@gmail.com>
2021-07-06 22:40:53 -0500

    first commit

---
+++ function-basics-2.py
@@ -0,0 +1,21 @@
+
+
+def countdown(number):
+    for x in range(number,0,-1):
+        print(x)
+
+countdown(10)
+
+def print_and_return(x,y):
+    print(x)
+    return y
+
+print_and_return(5,88)
+
+
+def first_plus_length(list):
+    x = first_plus_length[0] + len (first_plus_length)
+    return x
+
+
+first_plus_length([1,6,8,54,9])
